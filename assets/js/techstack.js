fba6bd6bddd8a22b946b65f6e374c3c44e545a98
namous68 <vnesys@gmail.com>
2023-09-29 11:30:22 +0100

    costum

---
+++ assets/js/techstack.js
@@ -0,0 +1,77 @@
+AOS.init();
+
+//  Tech Stacks cards
+
+const techStackCards = document.querySelector(".techstack-box");
+const techStack = [
+  {
+    langImage: "assets/images/techstack-page/html.png",
+    langName: "HyperText Markup Language",
+    langDesc: "<li>The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser</li>",
+  },
+  {
+    langImage: "assets/images/techstack-page/css.png",
+    langName: "Cascading Style Sheets",
+    langDesc: "<li>Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML</li>",
+  },
+  {
+    langImage: "assets/images/techstack-page/javascript.png",
+    langName: "JavaScript",
+    langDesc: "<li>JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS</li>",
+  },
+  
+  
+  
+  {
+    langImage: "assets/images/techstack-page/git.png",
+    langName: "Git",
+    langDesc: "<li>Git is software for tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development.</li>",
+  },
+  {
+    langImage: "assets/images/techstack-page/bootstrap.png",
+    langName: "Bootstrap",
+    langDesc: "<li>Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development</li>",
+  },
+  {
+    langImage: "assets/images/techstack-page/react.png",
+    langName: "React",
+    langDesc: "<li>React is a free and open-source front-end JavaScript library for building user interfaces based on UI components</li>",
+  },
+  {
+    langImage:"assets/images/techstack-page/symfony.png",
+    langName:"Symfony",
+    langDesc:"<li>Symfony: An open-source PHP web application framework, promoting modular design, MVC architecture, dependency injection, and Twig templating for web development.</li>"
+  },
+  {
+    langImage:"assets/images/techstack-page/php.png",
+    langName:"Php",
+    langDesc:"<li>Server side scripting language, widely used for web development. Execute on web servers to creat dynamic web content and interact with database.</li>"
+  },
+];
+
+const displayTechStacksCards = () => {
+  const entireCardTemplate =
+  techStack.map((stack)=> {
+      return `        
+    <div class="row page-content techstackcards" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="0" data-aos-duration="400"> 
+        <div class="tech_card">
+            <div class="card_img">
+                <img src ="${stack.langImage}" class="featured_image">
+            </div>
+            <div class="card_header">
+                <header>
+                    <div class="text-center langName">
+                        <h4>${stack.langName}</h4>
+                    </div>
+                </header>
+                <ul class="description">
+                ${stack.langDesc}
+            </ul>
+            </div>
+        </div>
+    </div>
+      `}).join('');
+  techStackCards.innerHTML = entireCardTemplate;
+};
+// displayTechStacksCards(techStack)
+document.addEventListener("DOMContentLoaded", displayTechStacksCards);
